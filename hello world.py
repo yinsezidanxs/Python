45e6a887ae1a9124695bcb8f3164d31b43687835
yinsezidanxs <13051587802@163.com>
2018-12-25 12:38:58 +0800

    a new file

---
+++ hello world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" This is the the standard beginning of the Python program """
+
+__author__ = 'Shu Xu'
+
+if __name__ == '__main__':
+    print('hello world')
