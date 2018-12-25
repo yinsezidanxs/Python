a358d4ac3aaf0028a4662622b26ea01153e898cb
yinsezidanxs <13051587802@163.com>
2018-12-25 12:21:00 +0800

    delete blank line

--- UI/pytoexe.py
+++ UI/pytoexe.py
@@ -6,7 +6,6 @@ from PyInstaller.__main__ import run
 # --icon：图标 
 # --noupx：不用upx压缩 
 # --clean：清理掉临时文件
-
 if __name__ == '__main__':
     opts = ['-F', '-y', '-w', '--paths=D:\\工作\\3 - 算法程序 - Python\\UI' '--noupx', '--clean', 'template.py']
     run(opts)
